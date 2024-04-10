dd69bd027a5965ecc631e5011d2da7d87254a3fa
LeonardoAir <leonardo.lopes@outlook.fr>
2024-04-10 03:28:23 +0200

    done

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const allQuestions = document.querySelectorAll('.question_answer_container');
+const allQuestions = document.querySelectorAll('.question_and_answer_container');
 
 allQuestions.forEach((question) => {
     question.addEventListener('click', (event) => {
         const icon = question.querySelector('.answer')
+        const image = event.currentTarget.querySelector('.question_tab_icon')
         icon.classList.toggle('answer_active')
+        console.log(event.currentTarget)
         
-        // if(answer.hidden === true){
-        //     img.src = './assets/images/icon-plus.svg';
-
-        // } else if(answer.hidden === false){
-        //     img.src = './assets/images/icon-minus.svg';
-        // }
-
-        // const container = event.currentTarget.parentElement.parentElement;
-        // container.querySelector('.answer').hidden = false;
+        if (icon.classList.contains('answer_active')){
+            image.src = ("./assets/images/icon-minus.svg")
+        } else {
+            image.src = ("./assets/images/icon-plus.svg")
+        }
     })
 })
